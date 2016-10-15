ba89a42a6c06c2b29d05fc7481032d2be3a5fc7b
Riza Fahmi <rizafahmi@gmail.com>
2016-10-15 18:25:17 +0700

    Refactor the Header component

--- src/Header.js
+++ src/Header.js
@@ -3,8 +3,35 @@ import React, {Component} from 'react'
 class Header extends Component {
   render() {
     return (
+      <div>
+        <Total />
+        <Title />
+      </div>
+    )
+  }
+}
+
+class Title extends Component {
+  render() {
+    return (
       <div className="header">
         <h1>Leaderboard</h1>
+      </div>
+    )
+
+  }
+}
+
+class Total extends Component {
+  render() {
+    return (
+      <div>
+        <div className="total-player">
+          Players: 3
+        </div>
+        <div className="total-point">
+          Total Point: 100
+        </div>
       </div>
     )
   }
